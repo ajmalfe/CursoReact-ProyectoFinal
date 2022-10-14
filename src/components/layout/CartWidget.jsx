e52b40e63c18fd1502031f4b44c4346a9be20cc8
Alejandro Malfetano <ajmalfe@users.noreply.github.com>
2022-10-14 18:01:47 -0300

    commit4

--- src/components/layout/CartWidget.jsx
+++ src/components/layout/CartWidget.jsx
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { CarritoContext } from '../../context/CarritoContext';
 
-const CartWidget = () => {
+function CartWidget() {
 
-    const [contador, setContador] = useState(0);
+    const {carrito} = useContext(CarritoContext);
+    const [valor, setValor] = useState(0);
+
+    const cambiarValor = () => {
+        let sum = carrito.reduce(function(prev, current) {
+            return prev + current.cantidad;
+          }, 0);
+
+        setValor(sum);
+    }
 
     return (
         <>
             <Link className='btn btn-secondary' to="/carrito">
                 <i className="fas fa-shopping-cart"></i>
             </Link>
+            <button onClick={() => cambiarValor()}>PRUEBA</button>
             <span className='badge bg-danger mb-3 l-10'>
-                {contador}
+                {valor}
             </span>
         </>
     );
